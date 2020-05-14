29bfff25cd65fe9418415ffa6abdd20cfc8d1515
MayuriJR <1260610535@qq.com>
2020-05-14 14:42:42 +0800

    update

--- Tools/sensor_data_processing_tool/experiment.py
+++
@@ -1,6 +0,0 @@
-from pricipitation.filter_data import DataFilter
-
-src_path = r"C:\Users\Administrator\Desktop\BOut"
-dest_path = src_path + "Final"
-dfr = DataFilter(src_path, dest_path)
-dfr.run()
